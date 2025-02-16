c799ef0141a9b21c33b227ddd531ad8b3f695005
Axel Lorens <axelnlorens@gmail.com>
2025-02-16 13:20:44 -0800

    Initial Commit

---
+++ GlobalRecipe/GlobalRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalRecipeApp.swift
+//  GlobalRecipe
+//
+//  Created by Axel Lorens on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GlobalRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
